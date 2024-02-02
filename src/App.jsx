538619fcdd23d7cb2d7bb85e0d3d117f01deca03
Roman <romchikus1987@gmail.com>
2024-02-02 16:06:16 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
 import { useQuery, useApolloClient } from '@apollo/client'
 import Persons from './components/Persons'
